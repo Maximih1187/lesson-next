044e9cacb442b8ccaf1f7851f774cdcb7ca2f1a2
Maximka <benko_0287.mail.ru>
2025-01-20 18:26:23 +0300

    final

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+      title: 'Customers | Acme Dashboard',
+};
+
+
+
 export default function Customers() {
       return <p>Customers Page</p>;
 }
---
+++ auth.config.ts
@@ -0,0 +1,24 @@
+import type { NextAuthConfig } from "next-auth";
+
+export const authConfig = {
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
+
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+
+        return false; // Перенаправлять не прошедших проверку пользователей на страницу входа в систему
+      } else if (isLoggedIn) {
+        console.log("object");
+        return Response.redirect(new URL("/dashboard", nextUrl));
+      }
+      return true;
+    },
+  },
+  providers: [], // Add providers with an empty array for now
+} satisfies NextAuthConfig;
